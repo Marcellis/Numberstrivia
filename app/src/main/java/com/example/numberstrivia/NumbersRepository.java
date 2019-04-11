f2a759418aa1fcb8ea13ca51461a3cc4db0ce678
Marcellis <m.m.c.m.marcellis@hva.nl>
2019-04-11 13:24:13 +0200

    Initial commit

---
+++ app/src/main/java/com/example/numberstrivia/NumbersRepository.java
@@ -0,0 +1,15 @@
+package com.example.numberstrivia;
+
+import retrofit2.Call;
+
+public class NumbersRepository {
+
+    private NumbersApiService numbersApiService = NumbersApi.create();
+
+    public Call<Trivia> getRandomNumberTrivia() {
+        return numbersApiService.getRandomNumberTrivia();
+    }
+
+}
+
+
